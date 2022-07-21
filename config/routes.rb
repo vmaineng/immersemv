6a5514eb6d943d7d218b8ee1d4b68a75565c6ce7
Mai Vang <vmaineng@gmail.com>
2022-07-21 14:43:32 -0700

    Configured client-side routing

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get '/hello', to: 'application#hello_world'
+
+  # Routing logic: fallback requests for React Router.
+  # Leave this here to help deploy your app later!
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+
 end
